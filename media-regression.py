e6e949bd360be345aa02046e4c993e16d02f26e4
milad <seyeadmiladnouri@gmail.com>
2025-12-29 16:08:16 +0330

    use-missingno

--- media-regression.py
+++ media-regression.py
@@ -1,5 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import missingno
 
 data =pd.read_csv('Advertising.csv',usecols=['TV','radio','newspaper','sales'])
+df=pd.DataFrame(data)
+# print(df.shape)
+# print(df.columns)
+# print(df.dtypes)
+# print(df.describe())
+print(df.info)
+print(df.isnull().sum())
+missingno.matrix(df)
+plt.show()
+
