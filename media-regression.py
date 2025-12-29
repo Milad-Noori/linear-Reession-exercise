fc925f9ab9c0efb5d2be34f826145a1811a6a299
milad <seyeadmiladnouri@gmail.com>
2025-12-29 16:23:12 +0330

    mean-benefits

--- media-regression.py
+++ media-regression.py
@@ -5,12 +5,23 @@ import missingno
 
 data =pd.read_csv('Advertising.csv',usecols=['TV','radio','newspaper','sales'])
 df=pd.DataFrame(data)
+
+############################################## understanding data
 # print(df.shape)
 # print(df.columns)
 # print(df.dtypes)
 # print(df.describe())
-print(df.info)
-print(df.isnull().sum())
-missingno.matrix(df)
-plt.show()
+# print(df.info)
+# print(df.isnull().sum())
+# missingno.matrix(df)
+# plt.show()
+##############################################
+df["benefit"] = 150-df['TV']
+print(df.head(10).to_string())
+
+
+
+
+
+
 
